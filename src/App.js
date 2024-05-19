29a244bd25a8f6ac2c092f1b41f8f9c5cb0cb002
Juri Dedej <juridedej552@gmail.com>
2024-05-19 18:06:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // Importing the React library
+import Nav from "./Nav"; // Importing the Nav component from the Nav.js file
+import Main from "./Main"; // Importing the Main component from the Main.js file
+import SecondPart from "./Second-part"; // Importing the SecondPart component from the Second-part.js file
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importing the Bootstrap CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Rendering the Nav component */}
+      <Nav />
+      {/* Rendering the Main component */}
+      <Main />
+      {/* Rendering the SecondPart component */}
+      <SecondPart />
     </div>
   );
 }
 
-export default App;
+export default App; // Exporting the App component as the default export
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // Importing the React library
+import ReactDOM from 'react-dom/client'; // Importing the ReactDOM library
+import './index.css'; // Importing the main CSS file for styling
+import App from './App'; // Importing the main App component
+import reportWebVitals from './reportWebVitals'; // Importing a function for reporting web vitals
 
+// Creating a root instance for the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Rendering the App component within a React.StrictMode element
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +16,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); // Calling the reportWebVitals function to start measuring performance
